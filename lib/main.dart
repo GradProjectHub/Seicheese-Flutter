5be52b5b72c73f1313cf2c6d193799dc7b0b19c9
takosan <takoocto0428@gmail.com>
2024-10-18 12:31:31 +0900

    logをSignに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:seicheese/screens/login_screen.dart';
+import 'package:seicheese/screens/Signin_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(), // constを追加
+      home: const SigninScreen(), // constを追加
     );
   }
 }

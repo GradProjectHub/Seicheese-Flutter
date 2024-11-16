ce0b75e45049f95019230aef03b67da61fb17c3a
Ryota Kuzuya <git@example.com>
2024-11-16 09:45:28 +0900

    main.dartに.envの読み込みを追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: ".env"); // この1行を追加
   runApp(MyApp());
 }
 
